5a12562034cd38663f9d96df02201b149ce5f650
guuiij <guilherme.jadson@hotmail.com>
2025-11-30 18:48:54 -0300

    adicionado método que salva dados de usuário

--- src/main/java/com/javanauta/usuario/infrastructure/entity/Telefone.java
+++ src/main/java/com/javanauta/usuario/infrastructure/entity/Telefone.java
@@ -1,25 +1,21 @@
 package com.javanauta.usuario.infrastructure.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Builder
 @Table(name = "telefone")
 public class Telefone {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "numero")
-    private Long numero;
-    @Column(name = "complemento",length = 10)
-    private String complemento;
+    private String numero;
     @Column(name = "ddd",length = 10)
     private String ddd;
 
